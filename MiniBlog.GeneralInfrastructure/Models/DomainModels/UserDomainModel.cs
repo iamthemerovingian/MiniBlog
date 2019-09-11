f9e2ad2877b51f010d2ac23fd1713c8639f87f6b
milinda <milinda.seneviratne@chilindo.com>
2019-09-11 14:37:45 +0700

    Set up the initial project structure with sepration of concerns. Added Swagger Middleware to act as API testing and documentation

---
+++ MiniBlog.GeneralInfrastructure/Models/DomainModels/UserDomainModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBlog.GeneralInfrastructure.Models.DomainModels
+{
+    public class UserDomainModel
+    {
+        public string UserName { get; set; }
+    }
+}
